7a5acd82b13e744dd1e2f12a9dc6e8d1a3b84592
jeels-donga <jeelsdonga2017@gmail.com>
2024-02-06 14:30:19 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
-import Product from './Pages/Product';
+// import Product from './Pages/Product';
+import Product1 from './Pages/Product1';
 function App() {
   return (
     <Routes>
       <Route path='/:page?' element={<Product />} />
+      {/* <Route path='/:page?' element={<Product1 />} /> */}
     </Routes >
   );
 }
