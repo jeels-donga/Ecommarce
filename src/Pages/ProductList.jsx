a42b99704707c8c5aeae271085bf461f63b968be
jeels-donga <jeelsdonga2017@gmail.com>
2024-02-16 16:30:40 +0530

    responsive setup

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -65,33 +65,51 @@ function Product1() {
     return (
         <div>
             <Header />
-            {/* <div>
-                <div className='Home-S2'>
-                    <div className='Sub-Home-S2'>
-                        <div>
-                            <Pagination NextPage={() => ChangeNextPage(currentPage)} PreviousPage={() => ChangePreviousPage(currentPage)} Page={currentPage} />
-                        </div>
-                    </div>
-                    <div className='Sub-Home-S2'>
-                        <div className='Head'>
-                            <h1>Products</h1>
-                        </div>
-                    </div>
-                    <div className='Sub-Home-S2'>
-                        <div className='limit'>
-                            <div className='d-flex justify-content-end mt-3'>
-                                <select id="cars" onChange={receiveDataFromChild}>
-                                    <option value="12" >12</option>
-                                    <option value="8">8</option>
-                                    <option value="18" >18</option>
-                                    <option value="24" >24</option>
-                                </select>
-                            </div>
-                        </div>
+            <div className='Section2'>
+                <div className='Sub-Home-S2'>
+                    <Pagination NextPage={() => ChangeNextPage(currentPage)} PreviousPage={() => ChangePreviousPage(currentPage)} Page={currentPage} />
+                </div>
+                <div className='title'>
+                    <h1>Products</h1>
+                </div>
+                <div className='limit'>
+                    <div className='d-flex justify-content-end mt-3'>
+                        <select id="cars" onChange={receiveDataFromChild}>
+                            <option value="12" >12</option>
+                            <option value="8">8</option>
+                            <option value="18" >18</option>
+                            <option value="24" >24</option>
+                        </select>
                     </div>
                 </div>
             </div>
-            <div className='product'>
+            <div className='Product-list'>
+                {
+                    product.map((e, i) => {
+                        return (
+                            <div className='product-box' key={i}>
+                                <Link to={`/Product/${e.id}`} className='link '>
+                                    <div className="card-img-top ">
+                                        <img src={e.images[0]} className="img2" alt="..." />
+                                    </div>
+
+                                    <div className='card-text'>
+                                        <h3>{e.title}</h3>
+                                    </div>
+                                    <div className='card-text'>
+                                        <p>Price:-{e.price}</p>
+                                    </div>
+                                    <div className='card-text'>
+                                        <button className='productlist-btn'><Link to={`/Product/${e.id} `} className='link'>More</Link></button>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+            {/* <div className='product'>
                 {
                     product.map((e, i) => {
                         return (
@@ -116,10 +134,10 @@ function Product1() {
                         )
                     })
                 }
-            </div>
-            <div className='pagination'>
-                <Pagination NextPage={() => ChangeNextPage(currentPage)} PreviousPage={() => ChangePreviousPage(currentPage)} Page={currentPage} />
             </div> */}
+            <div className='Section2'>
+                <Pagination NextPage={() => ChangeNextPage(currentPage)} PreviousPage={() => ChangePreviousPage(currentPage)} Page={currentPage} />
+            </div>
         </div >
     )
 }
