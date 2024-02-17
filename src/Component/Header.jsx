225b6a7b8f3e2172bf9d2a8a9423320c26fd1f70
jeels-donga <jeelsdonga2017@gmail.com>
2024-02-17 10:19:15 +0530

    header responsive

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -37,41 +37,6 @@ function Header() {
 
                 </div>
             </div >
-            {/* <div className='Header'>
-                <div>
-                    <h1> <Link to={"/"} className='a1'>LOGO</Link></h1>
-                </div>
-                <div className='menu-section'>
-                    <ul><li><Link to={"/"} className='a1'>Home</Link></li></ul>
-                </div>
-                <div className='search-section'>
-                  
-                </div>
-            </div>
-            <div className='Header-rs'>
-                <div className='menu1'>
-                    <h1> <Link to={"/"} className='a1'>LOGO</Link></h1>
-                </div>
-                <div className='menu'>
-                    <button></button>
-                </div>
-            </div>
-            <div className=' Header-rs menu'>
-                <div>
-                    <div className='menu'>
-                        <li><Link to={"/"} className='a1'>Home</Link></li>
-                    </div>
-                    <div className='menu'>
-                        <input type="text" value={search} onChange={(e) => SetSerach(e.target.value)}
-                            placeholder="Search for a product" className='input' />
-                        <button className='btn1'><Link to={`/Search/${search}`} className='a1'>submit</Link></button>
-                    </div>
-                    <div className='menu'>
-                        <Link to={'/SingleProduct/CartList'} className='a1'> <i className="fa fa-shopping-cart cart-icon" ></i> </Link>
-                    </div>
-                </div>
-
-            </div> */}
         </>
     )
 }
